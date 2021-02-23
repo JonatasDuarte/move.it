beae5c87e2e43b0f1fcb6bc03343f0a8f653f72b
JonatasDuarte <42140987+JonatasDuarte@users.noreply.github.com>
2021-02-23 15:57:23 -0300

    Cronometro adicionado |Componente funcional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/CountDown";
 import {ExperienceBar} from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div className={styles.container}>
-    <ExperienceBar />
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+      <ExperienceBar />
 
-    <section>
-      <div>
-        <Profile/>
-        <CompletedChallenges/>
-      </div>
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+          <Countdown/>
+        </div>
+        <div>
 
-      <div></div>
-    </section>
-  </div>
+        </div>
+      </section>
+    </div>
   )
 }
