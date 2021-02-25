e99258304233249d811fde1ccb4b78f36d2b2262
JonatasDuarte <42140987+JonatasDuarte@users.noreply.github.com>
2021-02-25 20:10:49 -0300

    Reset do cronometro |Implementado

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,22 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox(){
-  const {activeChallenge, resetChallenge, completeChallenge} = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallenge} = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handledChallengeSucceeded(){
+    completeChallenge();
+    resetCountdown();
+  }
   
+  function handledChallengeFailed(){
+    resetChallenge();
+    resetCountdown();
+  }
+
   return(
     <div className={styles.challengeBoxContainer}>
       { activeChallenge ? (
@@ -21,14 +33,14 @@ export function ChallengeBox(){
             <button 
               type='button'
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handledChallengeFailed}
             >
                 Falhei
             </button>
             <button 
               type='button'
               className={styles.challengeSucceededButton}
-              onClick={completeChallenge}
+              onClick={handledChallengeSucceeded}
             >
               Completei
             </button>
